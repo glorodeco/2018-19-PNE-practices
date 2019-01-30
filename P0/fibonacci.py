e97d3584b86677ef83308386e1c1ae4951fe41d1
glorodec <glorodeco@gmail.com>
2019-01-30 17:46:30 +0100

    fibonacci serie

---
+++ P0/fibonacci.py
@@ -0,0 +1,19 @@
+def fibonacci(n):
+    position_1 = 0
+    position_2 = 1
+    actual_number = 0
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        for i in range(n-2):
+            actual_number = position_1 + position_2
+            position_1 = position_2
+            position_2 = actual_number
+        return actual_number
+
+
+term = int(input("Your term is: "))
+solution = fibonacci(term)
+print("The value  is ", solution)
