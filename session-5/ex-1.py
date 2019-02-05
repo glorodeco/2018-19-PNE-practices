170ee5f20effab7415d813e63d725bf5ed781f62
glorodec <glorodeco@gmail.com>
2019-02-05 15:35:32 +0100

    dna count bases

---
+++ session-5/ex-1.py
@@ -0,0 +1,30 @@
+def count_a(seq):
+    """""Counting the number of As in the sequence"""
+
+
+   # Counter fot the As
+    result=0
+
+
+    for b in seq:
+        if b=='A':
+            result+=1
+
+    # Return the result
+    return result
+
+
+# Main programme
+s="AGTACACTGGT"
+na=count_a(s)
+print("The number of As is: {}".format(na))
+
+# Calculate the total sequence lenght
+tl=len(s)
+
+# Calculate the percentage of As in the sequence
+
+perc=round(100.0*na/tl,1)
+
+print('The total length is: {}'.format(tl))
+print('the percentage of As id: {}%'.format(perc))
