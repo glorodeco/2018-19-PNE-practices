ea0428be6eb0fcdfcc21a154ea62d369c42faaea
glorodec <glorodeco@gmail.com>
2019-02-20 17:23:12 +0100

    Pract. 2

---
+++ P2 Folder/client.py
@@ -0,0 +1,24 @@
+# WE ARE PROGRAMMING OUR FIRST CLIENT
+
+import socket
+
+# CREATE A SOCKET FOR COMMUNICATING WITH THE SERVER
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    PORT = 8081
+
+    IP="212.128.253.69"
+
+    s.connect((IP, PORT))
+
+    holi = input('Your sequence is:')
+
+    s.send(str.encode(holi))
+
+    msg = s.recv(2048).decode('utf-8')
+    print('Message from the server')
+    print(msg)
+    s.close()
+
+
