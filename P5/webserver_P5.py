ba08e96207be161dbd0abf39896d7051933e053c
glorodec <glorodeco@gmail.com>
2019-03-11 13:36:26 +0100

    Pract. 5

---
+++ P5/webserver_P5.py
@@ -0,0 +1,67 @@
+import http.server
+import socketserver
+import termcolor
+
+
+PORT = 8080
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        print("GET Received")
+
+
+        termcolor.cprint(self.requestline, 'green')
+        print("Request line:" + self.requestline)
+        print("     Cnd:    " + self.command)
+        print("Path:        " + self.path)
+
+
+        if self.path == "/":
+            with open("index.html", "r") as f:
+                contents = f.read()
+                f.close()
+        elif self.path == "/blue":
+            with open("blue.html", "r") as f:
+                contents = f.read()
+                f.close()
+        elif self.path == "/pink":
+            with open("pink.html", "r") as f:
+                contents = f.read()
+                f.close()
+
+        else:
+            with open("error.html", "r") as f:
+                contents = f.read()
+                f.close()
+
+        self.send_response(200)  # -- Status line: OK!
+
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+
+Handler = TestHandler
+
+#  Socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
